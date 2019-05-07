d6c336ab7d059c3395b133b03f460aef28bae39c
CEmmetHayes <ennishayes@hotmail.com>
2019-05-06 20:31:34 -0400

    fix requirements

--- mnetwebsite/settings.py
+++ mnetwebsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'not6w0zpy0&@#wj5naq&u=&)l3+9ri
 DEBUG = True
 #DEBUG = False
 #DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['salty-sands-81911.herokuapp.com']
 
 
 # Application definition
